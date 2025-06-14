68558975883536d1b48f6ff6ba539fdc676c5e7e
timothy adigun <adigun.timothy@gmail.com>
2025-06-14 22:49:35 +0100

    filter added

---
+++ eh/ch01/DoubleElems.hs
@@ -0,0 +1,24 @@
+module Main where
+
+doubleElems :: (Integral a) => [a] -> [a]
+doubleElems ls
+  | null ls = []
+  | otherwise = 2 * head ls : doubleElems (tail ls)
+
+doubleElems' :: (Integral a) => [a] -> [a]
+doubleElems' [] = []
+doubleElems' (x : xs) = 2 * x : doubleElems xs
+
+map' :: (t -> a) -> [t] -> [a]
+map' fn ls
+  | null ls = []
+map' fn (x : xs) = fn x : map' fn xs
+
+-- filter' :: (t -> a) -> [t] -> [a]
+filter' fn ls
+  | null ls = []
+filter' fn (x : xs)
+  | not $ fn x = filter' fn xs
+  | otherwise = x : filter' fn xs
+
+main = print $ doubleElems [1 .. 5]
