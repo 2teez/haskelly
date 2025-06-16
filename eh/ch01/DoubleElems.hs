6ea756d5ff96053bc472c8ae82004e2161b6f4fb
timothy adigun <adigun.timothy@gmail.com>
2025-06-16 07:54:17 +0100

    made various type of sum

--- eh/ch01/DoubleElems.hs
+++ eh/ch01/DoubleElems.hs
@@ -20,9 +20,25 @@ filter' fn (x : xs)
   | fn x = x : filter' fn xs
   | otherwise = filter' fn xs
 
+sip :: [a] -> [b] -> [(a, b)]
 sip [] [] = []
 sip _ [] = []
 sip [] _ = []
 sip (x : xs) (h : hs) = (x, h) : sip xs hs
+
+sumUp :: (Num a) => a -> [a] -> a
+sumUp initial ls
+  | null ls = 0
+  | otherwise = initial + head ls + sumUp initial (tail ls)
+
+sumUp' :: (Num t) => t -> [t] -> t
+sumUp' _ [] = 0
+sumUp' i (x : xs) = i + x + sumUp' i xs
+
+summe :: (Num t) => [t] -> t
+summe (x : xs) = sumi 0 (x : xs)
+  where
+    sumi _ [] = 0
+    sumi i (x : xs) = i + x + sumi i xs
 
 main = print $ doubleElems [1 .. 5]
