0f7e103bd4ef63f5b59b8a8d795b85c3a97b2eb8
timothy adigun <adigun.timothy@gmail.com>
2026-09-19 22:58:27 +0100

    recursion learning in haskell

--- lyah/ch03/Recur.hs
+++ lyah/ch03/Recur.hs
@@ -1,2 +1,20 @@
-module Main where
-main = print "Hello, World!"
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+-- using recursion
+factorial'' :: Integer -> Integer
+factorial'' 0 = 1
+factorial'' n = n * factorial'' (n - 1)
+
+-- using pattern matching and guards
+factorial' :: Integer -> Integer
+factorial' n
+    | n <= 0 = 1
+    | otherwise = n * factorial' (n - 1)
+
+-- using case ... of ...
+fac :: Integer -> Integer
+fac n = case n of
+    0 -> 1
+    1 -> 1
+    _ -> n * fac (n -1)
