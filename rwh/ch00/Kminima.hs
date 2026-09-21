d4a9ba1ff388c181c3a73ebc9879814d62284d84
timothy adigun <adigun.timothy@gmail.com>
2026-09-21 10:11:50 +0100

    trying out some code in haskell

--- rwh/ch00/Kminima.hs
+++ rwh/ch00/Kminima.hs
@@ -2,8 +2,14 @@
 --
 import Data.List
 
-minima k xs = take k $ sort xs
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = (quicksort [y | y <- xs, y <= x]) ++ [x] ++ (quicksort [y | y <- xs, y > x])
+
+-- use sort for Data.List avaliable instead of hand crafted quicksort here 
+minima k xs = take k $ quicksort xs 
 
 main :: IO ()
-main = do 
-    print $ minima 3 [3,8,9,1,5,0,7]
+main = do
+    print $ minima 3 ls
+        where ls = [3,8,9,1,5,0,7]
