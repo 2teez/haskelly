8425cfe69445e2753985a17e1d2b44484cbfb9a9
timothy adigun <adigun.timothy@gmail.com>
2026-09-22 09:32:35 +0100

    data keyword like oop language

--- rwh/ch02/Bookstore.hs
+++ rwh/ch02/Bookstore.hs
@@ -1,3 +1,15 @@
 -- file: Bookstore.hs
-module Main where
-main = print "Hello, World!"
+
+-- BookInfo new type, with type constructor
+data BookInfo = Book Int String [[Char]]
+  deriving (Show)
+
+-- MagazineInfo new type, with type constructor
+-- not same with BookInfo though look similar
+data MagazineInfo = Magazine Int String [String]
+  deriving (Show)
+
+main :: IO ()
+main = do
+  let myInfo = Book 90675438901 "Algebra of Programming" ["Java Gideon", "Rice Lice"]
+  print myInfo
