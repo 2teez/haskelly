e0ca5e0e936d1f45fc5d86c1421544f5f420c1a5
timothy adigun <adigun.timothy@gmail.com>
2026-09-22 03:16:27 +0100

    lastButOne and other functions

--- rwh/ch01/Take.hs
+++ rwh/ch01/Take.hs
@@ -13,6 +13,19 @@ myTake' n lst@(x : xs) =
     then []
     else x : myTake' (n - 1) xs
 
+last' :: [a] -> a
+last' [] = error "last': empty list"
+last' [x] = x
+last' (_ : xs) = last' xs
+
+lastButOne :: [a] -> a
+lastButOne [] = error "lastButOne: empty list"
+lastButOne [x, _] = x
+lastButOne (_ : xs) = lastButOne xs
+
 main :: IO ()
 main = do
   print $ myTake 3 [1, 2, 3, 4, 5]
+  print $ last' [1, 2, 3, 4, 5]
+  print $ lastButOne [1, 2, 3, 4, 5]
+  print $ lastButOne "JavaScript"
