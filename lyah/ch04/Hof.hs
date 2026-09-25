4936f0908a1c42fa91599f894f3a3494cb3a65c4
timothy adigun <adigun.timothy@gmail.com>
2026-09-25 17:28:39 +0100

    hof from haskell

--- lyah/ch04/Hof.hs
+++ lyah/ch04/Hof.hs
@@ -1,9 +1,27 @@
 -- map function re-wrote
 mapper :: (t -> a) -> [t] -> [a]
 mapper _ [] = []
-mapper f (x:xs) = f x : mapper f xs
+mapper f (x : xs) = f x : mapper f xs
 
 -- reduce
 reduce :: (Num a) => (t -> a) -> [t] -> a
 reduce _ [] = 0
-reduce f (x:xs) = (f x) + (reduce f xs)
+reduce f (x : xs) = f x + reduce f xs
+
+-- using section in haskell
+divideBy10 :: (Fractional a) => a -> a
+divideBy10 = (/ 10)
+
+-- check the letter
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A' .. 'Z'])
+
+-- function that accept a function and apply to a parameter
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+main :: IO ()
+main = do
+  print $ divideBy10 256
+  print $ isUpperAlphanum 'T'
+  print $ applyTwice (+ 3) 10
